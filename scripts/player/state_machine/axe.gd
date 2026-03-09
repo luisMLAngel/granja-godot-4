44a4b00a243b110bb2ccf8af35dcd8daed0d5359
Luis Angel Mendoza Lucio <luisangelmendozalucio@gmail.com>
2026-03-08 18:29:11 -0600

    feat: Implement axe tool animation and hitbox activation, and set player's default tool to "None"

--- scripts/player/state_machine/axe.gd
+++ scripts/player/state_machine/axe.gd
@@ -12,16 +12,14 @@ func _on_physics_process(_delta: float) -> void:
 	pass
 
 func _on_next_transitions() -> void:
-	if PlayerMovementUtils.is_moving(player.velocity):
-		transition.emit("WalkState")
-	elif InputUtils.is_action_pressed("action"):
-		player.set_can_move(false)
-		var current_tool_type = player.tool_controller.get_tool_type()
-		transition.emit(current_tool_type + "State")
-
+	if animation_finished:
+		transition.emit("IdleState")
 
 func _on_enter() -> void:
 	animation_finished = false
+	if not player.animation_player.animation_finished.is_connected(on_animation_finished):
+		player.animation_player.animation_finished.connect(on_animation_finished)
+		
 	if player.face_direction == Vector2.DOWN:
 		player.animation_player.play('tool_axe_down')
 	elif player.face_direction == Vector2.UP:
@@ -35,10 +33,11 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
+	if player.animation_player.animation_finished.is_connected(on_animation_finished):
+		player.animation_player.animation_finished.disconnect(on_animation_finished)
 	player.set_can_move(true)
 	animation_finished = false
 
-func on_animation_finished() -> void:
-	var anim: String = player.animation_player.animation
-	if anim.contains("axe"):
+func on_animation_finished(anim_name: String) -> void:
+	if "axe" in anim_name:
 		animation_finished = true
